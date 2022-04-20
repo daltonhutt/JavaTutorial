c4a8351e9eaad45b7fb990eeb3ff99094c2a227f
daltonhutt <103676585+daltonhutt@users.noreply.github.com>
2022-04-20 10:26:32 -0400

    Do While Loops.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,13 +9,11 @@ public class Main  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("quit")) {
+        do {
             System.out.println("Input: ");
             input = scanner.next().toLowerCase();
             System.out.println(input);
-
-
-        }
+        } while (!input.equals("quit"));
         }
     }
 
